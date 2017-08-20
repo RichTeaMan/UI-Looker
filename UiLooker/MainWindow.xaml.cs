ba6eca878738cad419ecd6bf9dff9719a85bd567
Thomas Holmes <thomasholmes@outlook.com>
2017-08-20 14:45:02 +0100

    Some way through implementing UI tree.

--- UiLooker/MainWindow.xaml.cs
+++ UiLooker/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Definitions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UiLooker.PresentationModels;
 
 namespace UiLooker
 {
@@ -22,7 +27,7 @@ namespace UiLooker
     {
 
         private ApplicationLoader _applicationLoader;
-
+        private HashSet<AutomationElement> autoSet = new HashSet<AutomationElement>();
         public MainWindow()
         {
             _applicationLoader = new ApplicationLoader();
@@ -40,15 +45,80 @@ namespace UiLooker
         private void LoadUiElements()
         {
             var mainWindow = _applicationLoader.FetchMainWindow();
+            var root = new TreeViewItem() { Header = mainWindow.Title };
 
             List<string> elementLines = new List<string>();
-            var elements = mainWindow.FindAllDescendants();
-            foreach(var element in elements)
+
+            var cacheRequest = new CacheRequest();
+            cacheRequest.TreeScope = TreeScope.Subtree | TreeScope.Element;
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.AutomationId);
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.ControlType);
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.ClassName);
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.Name);
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.LocalizedControlType);
+            cacheRequest.Add(mainWindow.Automation.PropertyLibrary.Element.FrameworkId);
+
+            List<ElementTreeView> elementTreeViewList = new List<ElementTreeView>();
+            
+            int childrenFilled = 0;
+            using (cacheRequest.Activate())
             {
-                elementLines.Add(element.ToString());
+                var elements = mainWindow.FindAll(TreeScope.Subtree, new TrueCondition());
+                //Console.WriteLine(elementCached.Properties.AutomationId.Value);
+                //Console.WriteLine(elementCached.Properties.Name.Value);
+
+                foreach (var e in elements)
+                {
+                    var elementTreeView = new ElementTreeView()
+                    {
+                        AutomationId = e.Properties.AutomationId.ValueOrDefault,
+                        Name = e.Properties.Name.ValueOrDefault,
+                        ClassName = e.Properties.ClassName.ValueOrDefault
+                    };
+                    elementTreeViewList.Add(elementTreeView);
+
+                    if (e.CachedChildren.Length > 0)
+                    {
+                        childrenFilled++;
+                    }
+                }
+            }
+
+            var elementsAndParents = new Dictionary<ElementTreeView, List<ElementTreeView>>();
+
+            foreach (var element in elementTreeViewList)
+            {
+                elementLines.Add($"{element.Name}");
+
+                List<ElementTreeView> parentList;
+                if (!elementsAndParents.TryGetValue(element, out parentList))
+                {
+                    parentList = new List<ElementTreeView>();
+                    elementsAndParents.Add(element, parentList);
+                }
+                //parentList.AddRange(element.Children);
             }
 
             uiResults.Text = string.Join(Environment.NewLine, elementLines);
+
+
+            treeview_ui.Items.Add(root);
+            AddChildElements(elementTreeViewList.First(), root, elementsAndParents);
+        }
+
+
+        private void AddChildElements(ElementTreeView element, TreeViewItem treeViewItem, Dictionary<ElementTreeView, List<ElementTreeView>> elementsAndParents)
+        {
+            List<ElementTreeView> elements;
+            if (elementsAndParents.TryGetValue(element, out elements))
+            {
+                foreach (var child in elements)
+                {
+                    var childTreeItem = new TreeViewItem() { Header = $"{child.Name}" };
+                    treeViewItem.Items.Add(childTreeItem);
+                    AddChildElements(child, childTreeItem, elementsAndParents);
+                }
+            }
         }
     }
 }
---
+++ UiLooker/PresentationModels/ElementTreeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiLooker.PresentationModels
+{
+    public class ElementTreeView
+    {
+        public string AutomationId { get; set; }
+        public string ControlType { get; set; }
+        public string ClassName { get; set; }
+        public string Name { get; set; }
+
+        public List<string> Children { get; set; }
+    }
+}
