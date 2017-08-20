4c470c25e5d37ecde211c6f328133d116f0c8f90
Thomas Holmes <thomasholmes@outlook.com>
2017-08-20 18:22:42 +0100

    Added MDI features to test project.

---
+++ TextBoxBoxSocial/Mdi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxBoxSocial
+{
+    public partial class Mdi : Form
+    {
+        public Mdi()
+        {
+            InitializeComponent();
+        }
+
+        private void textboxesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new Form1();
+
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void theOtherOneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new Form2();
+
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
